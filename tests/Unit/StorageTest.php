ee6c5c6c638b8189b6ff760390aca5f9ecd73ddc
Zdenyák József <zdenyakj@gmail.com>
2022-08-03 14:50:46 +0200

    Tests

--- tests/Unit/StorageTest.php
+++ tests/Unit/StorageTest.php
@@ -8,43 +8,8 @@ use \PHPUnit\Framework\TestCase as TestCase;
 
 class StorageTest extends TestCase
 {
-    public function test_storage_is_correct()
+    public function test()
     {
-        $storage = new Storage();
-
-        $product = new Product(
-            itemNumber: '2121', 
-            name:'Vans cipő', 
-            price: 200.30,
-            id: 1
-        );
-    
-        $product2 = new Product(
-            itemNumber: '3303', 
-            name:'Autó', 
-            price: 1000,
-            id: 3
-        );
-
-        $warehouse = new Warehouse(
-            name: 'Raktár 1', 
-            address: '7761 Kozármisleny Székely B. 35', 
-            capacity: 10,
-            id: 200,
-            storage: $storage
-        );
-        
-        $warehouse2 = new Warehouse(
-            name: 'Raktár 2', 
-            address: 'Budapest', 
-            capacity: 20,
-            id: 300,
-            storage: $storage
-        );
-
-        $warehouse->add(product: $product, quantity: 5);
-        $warehouse->add(product: $product2, quantity: 10);
-
-        // $this->assert
+        //
     }
 }
--- tests/Unit/WarehouseTest.php
+++ tests/Unit/WarehouseTest.php
@@ -1,9 +1,79 @@
 <?php
 declare(strict_types=1);
 
+use Classes\Warehouse;
 use \PHPUnit\Framework\TestCase as TestCase;
 
 class WarehouseTest extends TestCase
 {
+    public function test_attributes_are_set()
+    {
+        $warehouse = new Warehouse(
+            name: 'Raktár 1', 
+            address: '7761 Kozármisleny Székely B. 35', 
+            capacity: 10,
+            id: 200,
+            attributes: ['country' => 'Hungary']
+        );
     
+        $this->assertSame($warehouse->country, 'Hungary');
+    }
+
+    public function test_current_capacity_is_correct_on_creation()
+    {
+        $warehouse = new Warehouse(
+            name: 'Raktár 1', 
+            address: '7761 Kozármisleny Székely B. 35', 
+            capacity: 10,
+            id: 200,
+            attributes: ['country' => 'Hungary']
+        );
+    
+        $this->assertSame($warehouse->currentCapacity, 10);
+    }
+
+    public function test_current_capacity_is_set()
+    {
+        $warehouse = new Warehouse(
+            name: 'Raktár 1', 
+            address: '7761 Kozármisleny Székely B. 35', 
+            capacity: 10,
+            id: 200,
+            attributes: ['country' => 'Hungary']
+        );
+
+        $warehouse->setCurrentCapacity(5);
+    
+        $this->assertSame($warehouse->currentCapacity, 5);
+
+    }
+
+    public function test_is_full_is_correct()
+    {
+        $warehouse = new Warehouse(
+            name: 'Raktár 1', 
+            address: '7761 Kozármisleny Székely B. 35', 
+            capacity: 10,
+            id: 200,
+            attributes: ['country' => 'Hungary']
+        );
+
+        $warehouse->setCurrentCapacity(0);
+    
+        $this->assertTrue($warehouse->isFull());
+    }
+ 
+    public function test_is_empty_is_correct()
+    {
+        $warehouse = new Warehouse(
+            name: 'Raktár 1', 
+            address: '7761 Kozármisleny Székely B. 35', 
+            capacity: 10,
+            id: 200,
+            attributes: ['country' => 'Hungary']
+        );
+            
+        $this->assertTrue($warehouse->isEmpty());
+    }
+
 }
